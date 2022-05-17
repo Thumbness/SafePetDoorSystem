944d1c0948da6a4c386c6053a1fa6de611c07b75
Thumbness <face.first@hotmail.com>
2022-05-17 22:14:31 +1000

    add RFID, Button and LED functionality for Feeder Communication

--- src/DoorSystem.ino
+++ src/DoorSystem.ino
@@ -16,92 +16,108 @@
 
 int switchState;
 bool inside;
+int cntr;
 
 unsigned long uid;
 unsigned long feederCard;
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-void getCardID(const char *event, const char *data) {
+void getCardID(const char *event, const char *data)
+{
   Serial.println("Card Read from other device!");
   std::string temp = (std::string)data;
   feederCard = stoul(temp, 0, 10);
   Serial.println(feederCard);
 }
 
+void CheckPetStatus()
+{
+  if (cntr % 2 == 1)
+  {
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(YELLOW_LED, LOW);
+    if (uid == feederCard)
+    {
+      Particle.publish("Pet_status", "inside");
+    }
+  }
+  if (cntr % 2 == 0)
+  {
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, HIGH);
+    if (uid == feederCard)
+    {
+      Particle.publish("Pet_status", "outside");
+    }
+  }
+}
 
+void CheckforManualRecalibrate()
+{
+  switchState = digitalRead(SWITCH_PIN);
+  if (switchState == HIGH)
+  {
+    cntr += 1;
+    CheckPetStatus();
+    delay(2000);
+  }
+}
+
+void CheckCounter()
+{
+  if (cntr > 10)
+  {
+    cntr = 1;
+  }
+}
 
 // setup() runs once, when the device is first turned on.
-void setup() {
+void setup()
+{
   pinMode(SWITCH_PIN, INPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
+  cntr = 1;
 
- 
   mfrc522.setSPIConfig();
-  mfrc522.PCD_Init(); 
+  mfrc522.PCD_Init();
 
   Particle.subscribe("Card_ID", getCardID);
-  
 }
-
-
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  switchState = digitalRead(SWITCH_PIN);
-  if(switchState == HIGH)
+void loop()
+{
+
+  CheckforManualRecalibrate();
+  ReadCard();
+  CheckCounter();
+}
+
+void ReadCard()
+{
+  if (mfrc522.PICC_IsNewCardPresent())
   {
-    digitalWrite(GREEN_LED, HIGH);
-    Particle.publish("Press-A-Button", "inside");
+    cntr += 1;
+    uid = getID();
+    Serial.print("Card detected, UID: ");
     Serial.println(uid);
-    Serial.println(feederCard);
-    delay(1000);
   }
-
-  if(switchState == LOW)
-  {
-    digitalWrite(GREEN_LED, LOW);
-  }
-   
-  CardRead();
-  
 }
-
-
-void CardRead()
-{ 
-
-  if(mfrc522.PICC_IsNewCardPresent()) {
-      unsigned long uid = getID();
-      Serial.print("Card detected, UID: "); Serial.println(uid);
-      sendToFeeder();
-      if(uid != -1)
-      {
-        return;
-      }
-    }
-    
-}
-//get RFID tag ID
-unsigned long getID(){
-  if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
+// get RFID tag ID
+unsigned long getID()
+{
+  if (!mfrc522.PICC_ReadCardSerial())
+  { // Since a PICC placed get Serial and continue
     return -1;
   }
   unsigned long int hex_num;
-  hex_num =  mfrc522.uid.uidByte[0] << 24;
+  hex_num = mfrc522.uid.uidByte[0] << 24;
   hex_num += mfrc522.uid.uidByte[1] << 16;
-  hex_num += mfrc522.uid.uidByte[2] <<  8;
+  hex_num += mfrc522.uid.uidByte[2] << 8;
   hex_num += mfrc522.uid.uidByte[3];
   mfrc522.PICC_HaltA(); // Stop reading
+  CheckPetStatus();
   return hex_num;
 }
-
-void sendToFeeder()
-{
-  if(uid == feederCard)
-  {
-    Particle.publish("Pet_Status" "Movement");
-  }
-}
-
